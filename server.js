8ea26296c691f75b4dd0c3d9494cc8d3cc80796e
MarcoZ23 <zollomarco23@gmail.com>
2026-05-31 15:11:56 +0200

    add array

--- server.js
+++ server.js
@@ -9,3 +9,37 @@ app.listen(port, () => {
 app.get('/', (req, res) => {
     res.send('Server del mio Blog')
 })
+
+
+const posts = [
+    {
+        title: "Ciambellone",
+        content: "Questo è il contenuto del primo post",
+        image: "/img/ciambellone.jpeg",
+        tags: ["dolce", "colazione"]
+    },
+    {
+        title: "Cracker alla barbabietola",
+        content: "Questo è il contenuto del secondo post",
+        image: "/img/cracker_barbabietola.jpeg",
+        tags: ["salato", "spuntino"]
+    },
+    {
+        title: "Pane fritto dolce",
+        content: "Questo è il contenuto del secondo post",
+        image: "/img/pane_fritto_dolce.jpeg",
+        tags: ["dolce", "colazione"]
+    },
+    {
+        title: "pasta barbabietola",
+        content: "Questo è il contenuto del secondo post",
+        image: "/img/pasta_barbabietola.jpeg",
+        tags: ["salato", "pranzo"]
+    },
+    {
+        title: "Torta paesana",
+        content: "Questo è il contenuto del secondo post",
+        image: "/img/torta_paesana.jpeg",
+        tags: ["dolce", "dessert"]
+    }
+]
